f32f6feb844ce106e548e64f07ad66c58d28c1d0
Bharat Raj <128834374+bharattraj@users.noreply.github.com>
2024-05-12 09:08:02 +0000

    commit 4

---
+++ .devcontainer/js-basics/05_string.js
@@ -0,0 +1,12 @@
+const name="Bharat";
+const repoCount=0;
+console.log(name+repoCount+"Raj");
+//but the above way of concatanting strings is outdated , modern practice is to use backtick.
+
+console.log(`Hello my name is ${name} and my repo count is${repoCount} and lastname is${" Raj"} `);
+const str= new String("Bnarat");
+// console.log(str);
+// console.table(str);
+console.log(str[3]);
+console.log(str.toUpperCase());
+console.log(str.substring(0,4/*which is excluded */));
