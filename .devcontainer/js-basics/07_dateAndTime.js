67af48f7a55ec5f1a724acb06e4f8598198f33df
Bharat Raj <128834374+bharattraj@users.noreply.github.com>
2024-05-12 09:23:56 +0000

    commit 5

---
+++ .devcontainer/js-basics/07_dateAndTime.js
@@ -0,0 +1,16 @@
+//DATES
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getTime());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+let myCreatedDate=new Date("01-14-2024");
+
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
