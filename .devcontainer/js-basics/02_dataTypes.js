5b1516cebaca69ecebb266fc5c8d22c9931e366c
Bharat Raj <128834374+bharattraj@users.noreply.github.com>
2024-05-12 06:15:18 +0000

    commit2

---
+++ .devcontainer/js-basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //use to treat all JScode as newer version
+//alert(1+1)  //we are using node.js, not browser so alert doesnt work
+
+//code readablity should be present
+// double slash used for commenting
+/*used for 
+multiple line
+comments
+*/
+
+
+//DOCUMENTATION CAN BE READ FROM TC39
+
+//DATATYPE in JS
+//number
+//bigint
+//string
+//boolean
+//null:  standalone value
+//undefined => it means value is not assigned yet
+//symbol => unique
+
+
+//object
+
+
+//Using typeof to find the datatype of variable
+//type of null is object and undefined is undefined itself
+
